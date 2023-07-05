958392aff56acd7cd065c902e39bd758ffefec33
Henrique Bonfim <hp_bonfim@hotmail.com>
2023-07-05 00:19:43 -0400

    modify template

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import alienImage from './assets/alien_cloud.png'
 import './App.css'
 
 function App() {
@@ -9,25 +8,19 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={alienImage} className="logo" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello World</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <code className="read-the-docs">
+        Click <a href='https://hpbonfim.web.app/' target='_blank'>here</a> to learn more
+      </code>
     </>
   )
 }
