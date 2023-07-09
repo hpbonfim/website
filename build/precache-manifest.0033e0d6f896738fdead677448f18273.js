967f86bff216b1e017b8fc8da2c11654c10be7ee
Henrique Bonfim <hp_bonfim@hotmail.com>
2023-07-09 07:05:31 -0400

    update package
    using node v14

---
+++ build/precache-manifest.0033e0d6f896738fdead677448f18273.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307cd41a24253b7d199c46544826a874",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f76f7e37a32c2070d961",
+    "url": "/static/css/main.bd66ff41.chunk.css"
+  },
+  {
+    "revision": "4b8d34379d94cc48277f",
+    "url": "/static/js/2.ef616787.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ef616787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76f7e37a32c2070d961",
+    "url": "/static/js/main.4be125a8.chunk.js"
+  },
+  {
+    "revision": "d0ea3d71a37401a6ca99",
+    "url": "/static/js/runtime-main.0945728c.js"
+  },
+  {
+    "revision": "36774cd4cd0e8df24720e7b4a1debd0b",
+    "url": "/static/media/background.36774cd4.jpg"
+  }
+]);
--- src/components/projetos/index.tsx
+++ src/components/projetos/index.tsx
@@ -4,98 +4,98 @@ import { TiSocialGithub } from "react-icons/ti";
 import projectData from './projects.json'
 
 interface ProjectData {
-    id: number;
-    title: string;
-    text: string;
-    image: string;
-    link: string
+  id: number;
+  title: string;
+  text: string;
+  image: string;
+  link: string
 }
 
 const Projetos = () => {
-    const [project, setProjects] = useState<ProjectData[]>([])
-    const [arrowStyle, setArrowStyle] = useState<string>("⇉")
-    const [isToggled, setToggle] = useState<Boolean>(false)
-    const [view, setView] = useState<string>("none")
+  const [project, setProjects] = useState<ProjectData[]>([])
+  const [arrowStyle, setArrowStyle] = useState<string>("⇉")
+  const [isToggled, setToggle] = useState<Boolean>(false)
+  const [view, setView] = useState<string>("none")
 
-    useEffect(() => {
-        setProjects(projectData)
-        changeStyle(isToggled)
-    }, [isToggled])
+  useEffect(() => {
+    setProjects(projectData)
+    changeStyle(isToggled)
+  }, [isToggled])
 
-    function handleToggle(e: any) {
-        e.preventDefault()
-        return isToggled === true ? setToggle(false) : setToggle(true)
+  function handleToggle(e: any) {
+    e.preventDefault()
+    return isToggled === true ? setToggle(false) : setToggle(true)
+  }
+
+  function changeStyle(value: Boolean) {
+    if (value) {
+      setArrowStyle("⇊")
+      setView("")
+    } else {
+      setArrowStyle("⇉")
+      setView("none")
     }
- 
-    function changeStyle(value: Boolean) {
-        if (value) {
-            setArrowStyle("⇊")
-            setView("")
-        } else {
-            setArrowStyle("⇉")
-            setView("none")
-        }
-    }
+  }
 
-    const style = {
-        display: view
-    }
+  const style = {
+    display: view
+  }
 
 
-    return (
-        <div className="container">
-            <div className="projects">
-            <span onClick={handleToggle} >
-                    {arrowStyle} My little projects...
-                </span>
-                <hr></hr>
-                <div className="card" style={style}>
-                    {
-                        project.map(project => (
-                            <div key={project.id} className="card-list">
-                                <div className="card-header">
-                                    <img className="card-header-image" src={project.image} alt={project.title} />
-                                </div>
-                                <div className="card-footer">
-                                    <a href={project.link} rel="noopener noreferrer" target="_blank">
-                                        <span>
-                                            <TiSocialGithub />
-                                        </span>
-                                        <strong className="card-title">
-                                            Project {project.title}
-                                        </strong>
-                                    </a>
-                                </div>
-                                <div className="card-body">
-                                    <p className="body-content">{project.text}</p>
-                                </div>
-
-                            </div>
-                        ))
-                    }
-                    <div className="card-list">
-                        <header className="card-header">
-                            <img className="card-header-image" src={"https://raw.githubusercontent.com/hpbonfim/website/master/src/assets/projetos-page/projects800x600.jpg"} alt={"projects"} />
-                        </header>
-                        <div className="card-footer">
-                            <a href={"https://github.com/hpbonfim?tab=repositories"} rel="noopener noreferrer" target="_blank">
-                                <span>
-                                    <TiSocialGithub />
-                                </span>
-                                <strong>
-                                    Repositório...
-                                </strong>
-                            </a>
-                        </div>
-                        <div className="card-body">
-                            <h3>Confira outros projetos...</h3>
-                            <p className="body-content">Tenho diversos projetos open-source para qualquer pessoa contribuir ou ser contribuído com minhas soluções, desde API's até aplicativos completos. be my guest!</p>
-                        </div>
-                    </div>
+  return (
+    <div className="container">
+      <div className="projects">
+        <span onClick={handleToggle} >
+          {arrowStyle} My little projects...
+        </span>
+        <hr></hr>
+        <div className="card" style={style}>
+          {
+            project.map(project => (
+              <div key={project.id} className="card-list">
+                <div className="card-header">
+                  <img className="card-header-image" src={project.image} alt={project.title} />
                 </div>
+                <div className="card-footer">
+                  <a href={project.link} rel="noopener noreferrer" target="_blank">
+                    <span>
+                      <TiSocialGithub />
+                    </span>
+                    <strong className="card-title">
+                      Project {project.title}
+                    </strong>
+                  </a>
+                </div>
+                <div className="card-body">
+                  <p className="body-content">{project.text}</p>
+                </div>
+
+              </div>
+            ))
+          }
+          <div className="card-list">
+            <header className="card-header">
+              <img className="card-header-image" src={"https://raw.githubusercontent.com/hpbonfim/website/production/src/assets/projetos-page/projects800x600.jpg"} alt={"projects"} />
+            </header>
+            <div className="card-footer">
+              <a href={"https://github.com/hpbonfim?tab=repositories"} rel="noopener noreferrer" target="_blank">
+                <span>
+                  <TiSocialGithub />
+                </span>
+                <strong>
+                  Repositório...
+                </strong>
+              </a>
             </div>
+            <div className="card-body">
+              <h3>Confira outros projetos...</h3>
+              <p className="body-content">Tenho diversos projetos open-source para qualquer pessoa contribuir ou ser contribuído com minhas soluções, desde API's até aplicativos completos. be my guest!</p>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Projetos
