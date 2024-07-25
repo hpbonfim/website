54f4b3e983556ac123815c6eb874555a4a2599fc
Henrique Paulo Bonfim <40275173+hpbonfim@users.noreply.github.com>
2024-07-25 18:52:11 +0000

    fix: adjust ci/cd and update package

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  root: true,
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
@@ -8,7 +9,11 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   plugins: ["react-refresh", "lingui"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
   rules: {
-    "react-refresh/only-export-components": "warn",
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
   },
 };
